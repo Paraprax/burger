b5e322c18c405bb0305e12d73fe829cb1b60671f
Paraprax <parkerjohnston28@gmail.com>
2019-03-16 11:00:03 -0400

    added JAWS_DB code to connection.js for deployment on Heroku

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,20 @@
 
 //dependencies:
 var mysql = require("mysql");
+var connection;
 
 //defines the connection:
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "parkpass",
-  database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "parkpass",
+    database: "burger_db"
+  });
+};
 
 //makes the connection:
 connection.connect(function(err) {
