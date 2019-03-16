d42fb904dc60e1490bf23728da7be426103508d5
Paraprax <parkerjohnston28@gmail.com>
2019-03-16 01:19:16 -0400

    added burger-model function calls to appropriate routes in the burgerController

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -6,11 +6,44 @@ var burger = require("../models/burger.js"); //<- imports the burger-model to us
 var router = express.Router();
 
 //all our routes for interaction with this app:
-router.get({});
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = { //<- creates an object to send data to our handlebars pages(which require objects)
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
-router.post({});
+router.post("api/burgers", function(req, res){
+    burger.create(
+    [
+        "burger_name", "devoured"
+    ], 
+    [
+        req.body.burger_name, req.body.devoured
+    ], function (result) {
+        res.json({ id: result.insertId }); //sends back new burger's id
+    });
+});
 
-router.put({});
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+  
+    burger.update({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // if no rows were changed, then the ID must not exist here, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end(); //200-OK if good
+      }
+    });
+  });
 
 //export the router for the server.js to access
 module.exports = router;
