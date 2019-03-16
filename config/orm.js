65c83735fb9c2655535a511b365ed77f7f15c54b
Paraprax <parkerjohnston28@gmail.com>
2019-03-15 23:53:03 -0400

    added defined orm object with basic key/function pairs

---
+++ config/orm.js
@@ -0,0 +1,18 @@
+//imports the mysql connection:
+var connection = require("../config/connection.js");
+
+//ORM object for functions running sql-statement queries:
+var orm = {
+    selectAll: function() {
+        console.log("Select all burgers");
+    },
+    insertOne: function() {
+        console.log("Add a new burger");
+    },
+    updateOne: function() {
+        console.log("Update a burger");
+    }
+};
+
+// exports the ORM object for the burger.js model to access:
+module.exports = orm;
