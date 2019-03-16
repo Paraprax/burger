326d1bbf53eee96d7e00f0c6d643ae83d6cd8c15
Paraprax <parkerjohnston28@gmail.com>
2019-03-16 04:25:44 -0400

    added jQuery function to submit new burgers from the form to the page

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,7 +1,9 @@
-//note: all functions enclosed in one surrounding function which doesn't call until after the DOM is fully loaded to prevent errors:
+//note: all functions enclosed in this surrounding function which doesn't fire until after the DOM is fully loaded to prevent errors:
 $(function() {
-
+    
     //jQuery functions for event-handling:
+
+    //function to change a burger's status to 'devoured'
     $(".change-ate").on("click", function(event) {
         var id = $(this).data("id");
         var newDevour = $(this).data("newdevour");
@@ -22,4 +24,29 @@ $(function() {
             }
         );
     });
-});
+
+    //function to submit new burgers to the menu from the "Add A Burger" form:
+    $(".add-burger-form").on("submit", function(event) {
+        //preventDefault called first, to prevent empty forms being submitted as new burgers!
+        event.preventDefault();
+
+        //creates a new burger object with the user-submitted name, set to uneaten by default:
+        var newBurger = {
+            burger_name: $("#burg").val().trim(),
+            devoured: false
+        };
+
+        //sends the POST request containing our newBurger object:
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("Whipped up a new burger!")
+                //reloads the page to update the burger menu with the new data:
+                location.reload();
+            }
+        );
+    }); //<- end of final event-handling function
+        
+}); //<- End of over-arching function.
