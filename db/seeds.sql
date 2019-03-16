24e5027202e83bbec1148746527c0e309594bc55
Paraprax <parkerjohnston28@gmail.com>
2019-03-15 23:27:16 -0400

    wrote scheme and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('BeyondMeat Burger');
+INSERT INTO burgers (burger_name) VALUES ('Deluxo Vegan Supreme');
+INSERT INTO burgers (burger_name) VALUES ('Soy-Prime Vulcanburger');
+INSERT INTO burgers (burger_name) VALUES ('Yawthentic Yamburger');
+INSERT INTO burgers (burger_name) VALUES ('Portobello Fellow');
